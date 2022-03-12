728ecd6a7f6ad7bf24c4da757d301924b857ed96
althof3 <althoframdhan3@gmail.com>
2022-03-12 21:36:30 +0700

    add Chakra UI library

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ ingredients:
 - `redux-toolkit`
 - `redux-persist` w/ [`next-redux-wrapper`](https://github.com/kirill-konshin/next-redux-wrapper)
 - `typescript`
+- [`Chakra UI`](https://chakra-ui.com/guides/getting-started/nextjs-guide)
+
+How to run:
+- `yarn`
+- `yarn dev`
+- open `localhost:3000`
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from 'next/app';
 import { wrapper } from 'redux/store';
 import { useStore } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const store = useStore();
   return (
-    // @ts-ignore
-    <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
-      <Component {...pageProps} />
-    </PersistGate>
+    <ChakraProvider>
+      {/* @ts-ignore */}
+      <PersistGate persistor={store.__persistor} loading={<Component {...pageProps} />}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </ChakraProvider>
   );
 }
 
